d90aef04ed34bf3f9a6ff9e7e9177314ea14c208
Damn it, he's good <106484743+wolonee@users.noreply.github.com>
2025-03-19 00:55:54 +0400

    Update collectsystem.py

--- collectsystem.py
+++ collectsystem.py
@@ -1,7 +1,7 @@
-BOT_TOKEN="7100142932:AAH9UuFl7vWUh6WbJlUjXzFmJCLS3yJu0Do"
-DataUrl = "mongodb://expin1226707:.aRTTIG12267@91.107.123.97:37757/admin?authSource=admin&readPreference=secondary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
-cryptopayapi_token = "181985:AADE1Jktfvw9hoOq1QH2zW0A1Nk0QoZZ1d3"
-chan_id_BUY_TON = -1002005628043
-chan_id_SELL_TON = -1002042690097
-main_chan_id = -1002107123327
-chan_id_TOKENS = -1002117388362
+BOT_TOKEN="......."
+DataUrl = "......."
+cryptopayapi_token = "......."
+chan_id_BUY_TON = .......
+chan_id_SELL_TON = .......
+main_chan_id = .......
+chan_id_TOKENS = .......
